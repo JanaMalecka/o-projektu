20a241c3f62919ed1140b706a357fa0f573aa5f1
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-19 14:29:51 +0200

    responsive illustration reacts on click

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ for (let i = 0; i < navLinkEl.length; i++) {
     navEl.classList.add('nav-closed');
   });
 }
+/*..............responvivita obrazku na hlavni strance....*/
+
+const Polaroid = ({ polaroidText, imageText }) => {
+  let resultHTML = `
+  <div class="image-wrapper">
+    <img
+      class="illustration"
+      src="img/hradejov.png"
+      alt="picture of main Hradilov characters"
+    />
+    <div class="polaroid-text">${polaroidText}</div>
+    
+      <div class="image-text image-text--off">${imageText}
+    </div>
+ `;
+  return resultHTML + '</div>';
+};
+
+document.querySelector('.polaroid').innerHTML = Polaroid({
+  polaroidText: 'Click here to see more.',
+  imageText: "We can't show you more!",
+});
+
+const imageTextEl = document.querySelector('.image-text');
+
+document.querySelector('.polaroid').addEventListener('click', () => {
+  imageTextEl.classList.toggle('image-text--on');
+});
